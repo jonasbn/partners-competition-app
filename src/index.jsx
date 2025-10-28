edd8dfdf97cc6869a0982ca037b6c909603da70a
jonasbn <jonasbn@gmail.com>
2025-10-28 18:50:17 +0100

    Migrated from CRA to Vite

--- src/index.jsx
+++ src/index.jsx
@@ -2,8 +2,8 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './index.css';
-import App from './App';
-import { ThemeProvider } from './utils/ThemeContext';
+import App from './App.jsx';
+import { ThemeProvider } from './utils/ThemeContext.jsx';
 // Import i18n configuration
 import './utils/i18n';
 
